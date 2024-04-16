72c676ebe031ffc45de2f9099ccb9fa0df6695c1
George-Tharpe <gjt35703@uga.edu>
2024-04-16 16:25:27 -0400

    updated home screen

--- app/src/main/java/edu/uga/cs/rideshare/HomeScreenFragment.java
+++ app/src/main/java/edu/uga/cs/rideshare/HomeScreenFragment.java
@@ -5,8 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class HomeScreenFragment extends Fragment {
     public HomeScreenFragment() {
@@ -31,6 +37,140 @@ public class HomeScreenFragment extends Fragment {
                     .replace(R.id.fragment_container, new SplashFragment())
                     .commit();
         });
+
+        User currentUser = new User("current@gmail.com", "pass");
+        List<Ride> aList = new ArrayList<>();
+        aList.add(new Ride(new Date(), "Home", "not Home", currentUser, null));
+        aList.add(new Ride(new Date(), "Home", "not Home", currentUser, null));
+        aList.add(new Ride(new Date(), "Home", "not Home", currentUser, null));
+
+
+        LinearLayout aLayout = view.findViewById(R.id.a_rides_layout);
+
+        for (int i = aList.size() - 1; i >= 0; i--) {
+            Ride ride = aList.get(i);
+
+            // Create a TextView to display ride information
+            TextView textView = new TextView(getContext());
+            textView.setTextAppearance(getContext(), R.style.MyTextViewStyle);
+            textView.setText("\n\nRide Date: " + ride.date + "\n" + "To: " + ride.goingTo + ", From: " + ride.from);
+
+            // Create a Button to mark the ride as completed
+            Button button = new Button(getContext());
+            button.setTextAppearance(getContext(), R.style.MyButtonStyle);
+            button.setText("Ride Completed");
+
+            // Set an OnClickListener for the button to update ride status when clicked
+            button.setOnClickListener(v -> {
+                // Update ride status to completed
+                ride.rideCompletedDriver = true;
+                // Remove the button after it's clicked
+                aLayout.removeView(button);
+                aLayout.removeView(textView);
+            });
+
+            // Add the TextView and Button to the layout
+            aLayout.addView(textView);
+            aLayout.addView(button);
+        }
+
+
+        List<Ride> rList = new ArrayList<>();
+        rList.add(new Ride(new Date(), "Home", "not Home", currentUser, null));
+        rList.add(new Ride(new Date(), "Home", "not Home", currentUser, null));
+        rList.add(new Ride(new Date(), "Home", "not Home", currentUser, null));
+
+
+        LinearLayout rLayout = view.findViewById(R.id.r_rides_layout);
+        for (int i = rList.size() - 1; i >= 0; i--) {
+            Ride ride = rList.get(i);
+
+            // Create a TextView to display ride information
+            TextView textView = new TextView(getContext());
+            textView.setTextAppearance(getContext(), R.style.MyTextViewStyle);
+            textView.setText("\n\nRide Date: " + ride.date + "\n" + "To: " + ride.goingTo + ", From: " + ride.from);
+
+
+            Button updateButton = new Button(getContext());
+            updateButton.setTextAppearance(getContext(), R.style.MyButtonStyle);
+            updateButton.setText("Update");
+
+            Button deleteButton = new Button(getContext());
+            deleteButton.setTextAppearance(getContext(), R.style.MyButtonStyle);
+            deleteButton.setText("Delete");
+
+            updateButton.setOnClickListener(v -> {
+
+                // Remove the button after it's clicked
+                rLayout.removeView(updateButton);
+                rLayout.removeView(deleteButton);
+                rLayout.removeView(textView);
+            });
+
+
+
+            deleteButton.setOnClickListener(v -> {
+                rList.remove(ride);
+                // Remove the button after it's clicked
+                rLayout.removeView(deleteButton);
+                rLayout.removeView(updateButton);
+                rLayout.removeView(textView);
+            });
+
+            // Add the TextView and Button to the layout
+            rLayout.addView(textView);
+            rLayout.addView(updateButton);
+            rLayout.addView(deleteButton);
+        }
+
+
+        List<Ride> oList = new ArrayList<>();
+        oList.add(new Ride(new Date(), "Home", "not Home", currentUser, null));
+        oList.add(new Ride(new Date(), "Home", "not Home", currentUser, null));
+        oList.add(new Ride(new Date(), "Home", "not Home", currentUser, null));
+
+
+        LinearLayout oLayout = view.findViewById(R.id.o_rides_layout);
+        for (int i = oList.size() - 1; i >= 0; i--) {
+            Ride ride = oList.get(i);
+
+            // Create a TextView to display ride information
+            TextView textView = new TextView(getContext());
+            textView.setTextAppearance(getContext(), R.style.MyTextViewStyle);
+            textView.setText("\n\nRide Date: " + ride.date + "\n" + "To: " + ride.goingTo + ", From: " + ride.from);
+
+
+            Button updateButton = new Button(getContext());
+            updateButton.setTextAppearance(getContext(), R.style.MyButtonStyle);
+            updateButton.setText("Update");
+
+            Button deleteButton = new Button(getContext());
+            deleteButton.setTextAppearance(getContext(), R.style.MyButtonStyle);
+            deleteButton.setText("Delete");
+
+            updateButton.setOnClickListener(v -> {
+
+                // Remove the button after it's clicked
+                oLayout.removeView(updateButton);
+                oLayout.removeView(deleteButton);
+                oLayout.removeView(textView);
+            });
+
+
+
+            deleteButton.setOnClickListener(v -> {
+                oList.remove(ride);
+                // Remove the button after it's clicked
+                oLayout.removeView(deleteButton);
+                oLayout.removeView(updateButton);
+                oLayout.removeView(textView);
+            });
+
+            // Add the TextView and Button to the layout
+            oLayout.addView(textView);
+            oLayout.addView(updateButton);
+            oLayout.addView(deleteButton);
+        }
 
 
 
