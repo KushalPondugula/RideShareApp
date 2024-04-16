3814b44f045e1f49df03ce6637b0df1d511c74b3
George-Tharpe <gjt35703@uga.edu>
2024-04-16 12:37:04 -0400

    added splash screen

--- app/src/main/java/edu/uga/cs/rideshare/MainActivity.java
+++ app/src/main/java/edu/uga/cs/rideshare/MainActivity.java
@@ -3,11 +3,18 @@ package edu.uga.cs.rideshare;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Display the SplashFragment initially
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new SplashFragment())
+                .commit();
     }
 }
---
+++ app/src/main/java/edu/uga/cs/rideshare/SplashFragment.java
@@ -0,0 +1,57 @@
+package edu.uga.cs.rideshare;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class SplashFragment extends Fragment {
+    public SplashFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_splash, container, false);
+
+
+        // Find the start button
+        Button loginButton = view.findViewById(R.id.login_button);
+
+
+        // Set OnClickListener to start the quiz when the button is clicked
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace the SplashFragment with the QuizFragment
+                requireActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new QuizContainerFragment(countryData))
+                        .commit();
+            }
+        });
+
+        Button signUpButton = view.findViewById(R.id.signUp_button);
+
+        // Set OnClickListener to start the quiz when the button is clicked
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace the SplashFragment with the QuizFragment
+                requireActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new PastQuizFragment(countryData))
+                        .commit();
+            }
+        });
+
+
+        return view;
+    }
+
+}
+
+
