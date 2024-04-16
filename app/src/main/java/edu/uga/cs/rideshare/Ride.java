17ad5890745e7066b0a7de22d83edbebdda41229
George-Tharpe <gjt35703@uga.edu>
2024-04-16 13:57:45 -0400

    added the user and ride class

---
+++ app/src/main/java/edu/uga/cs/rideshare/Ride.java
@@ -0,0 +1,47 @@
+package edu.uga.cs.rideshare;
+
+import java.util.Date;
+
+public class Ride {
+    public Date date;
+    public String goingTo;
+    public String from;
+    public boolean driverAccepted;
+    public boolean riderAccepted;
+    public User driver;
+    public User rider;
+    public boolean rideCompletedDriver;
+    public boolean rideCompletedRider;
+
+    public Ride (Date date, String goingTo, String from, User driver, User rider) {
+        this.date = date;
+        this.goingTo = goingTo;
+        this.from = from;
+
+        if (driver != null && rider != null) {
+            riderAccepted = true;
+            driverAccepted = true;
+            this.driver = driver;
+            this.rider = rider;
+        } else if (driver == null && rider != null) {
+            driverAccepted = false;
+            riderAccepted = true;
+            this.rider = rider;
+        } else if (rider == null && driver != null){
+            riderAccepted = false;
+            driverAccepted = true;
+            this.driver = driver;
+        } else {
+            riderAccepted = false;
+            driverAccepted = false;
+        }
+    }
+
+    public void updatedCompletedDriver(boolean val){
+        rideCompletedDriver = val;
+    }
+    public void updatedCompletedRider(boolean val){
+        rideCompletedRider = val;
+    }
+
+}
---
+++ app/src/main/java/edu/uga/cs/rideshare/User.java
@@ -0,0 +1,22 @@
+package edu.uga.cs.rideshare;
+
+public class User {
+    public String email;
+    public String pass;
+    public int points;
+
+    public User(String email, String pass) {
+        this.email = email;
+        this.pass = pass;
+        this.points = 50;
+    }
+
+    public void addPoints() {
+        points += 50;
+    }
+
+    public void subtractPoints() {
+        points -= 50;
+    }
+
+}
